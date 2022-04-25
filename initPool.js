a186fe0c0c7dca2643e42ea58b01a9d5202c2c53
Liam Leung <liam.leung@binery.co>
2022-04-25 13:21:42 +0800

    feat: pool

--- initPool.js
+++ initPool.js
@@ -1,12 +1,27 @@
 import pg from 'pg';
 
 const { Pool } = pg;
-const pgConnectionConfigs = {
-  user: 'yickkiuliamleung',
-  host: 'localhost',
-  database: 'uploads',
-  port: 5432,
-};
+
+let pgConnectionConfigs;
+
+// test to see if the env var is set. Then we know we are in Heroku
+if (process.env.DATABASE_URL) {
+  // pg will take in the entire value and use it to connect
+  pgConnectionConfigs = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  };
+} else {
+  // this is the same value as before
+  pgConnectionConfigs = {
+    user: 'yickkiuliamleung',
+    host: 'localhost',
+    database: 'uploads',
+    port: 5432,
+  };
+}
 
 const pool = new Pool(pgConnectionConfigs);
 
