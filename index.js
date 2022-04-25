aad084e5bdc8dd373c6fa90e53c4eea3b4a69908
Liam Leung <liam.leung@binery.co>
2022-04-25 13:27:04 +0800

    feat: add s3

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ import express from 'express';
 import multer from 'multer';
 import path from 'path';
 import dotenv from 'dotenv';
+import aws from 'aws-sdk';
+import multerS3 from 'multer-s3';
+
 import pool from './initPool.js';
 
 const PORT = process.env.PORT || 3004;
+
+// intialise s3
+const s3 = new aws.S3({
+  accessKeyId: process.env.ACCESSKEYID,
+  secretAccessKey: process.env.SECRETACCESSKEY,
+});
 
 const envFilePath = '.env';
 dotenv.config({ path: path.normalize(envFilePath) });
@@ -18,7 +27,19 @@ const apiUrl = 'https://api.unsplash.com';
 
 // todo: set the name of the upload directory here
 
-const multerUpload = multer({ dest: 'uploads/' });
+const multerUpload = multer({
+  storage: multerS3({
+    s3,
+    bucket: 'liam-socket-demo',
+    acl: 'public-read',
+    metadata: (request, file, callback) => {
+      callback(null, { fieldName: file.fieldname });
+    },
+    key: (request, file, callback) => {
+      callback(null, Date.now().toString());
+    },
+  }),
+});
 const singleFileUpload = multerUpload.single('photo');
 
 // * Save file with original filename ie. hello.jpg
@@ -41,7 +62,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('uploads'));
 
-app.get('/recipe', (request, response) => {
+app.get('/', (request, response) => {
   response.render('upload');
 });
 
